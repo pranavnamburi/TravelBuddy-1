b3f13f8d99c484f1d38c363831f893c22d152a17
Mukesh Thatikonda <mukesh_thatikonda@srmap.edu.in>
2023-04-17 23:53:42 +0530

    My commit

--- src/main/java/com/groupnine/travelbuddy/HelloServlet.java
+++ src/main/java/com/groupnine/travelbuddy/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Welcome to Travel Buddy";
+        message = "Welcome to Travel Buddy School";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
