fcd0b77838e1ef39d0deec2210991c8ac71b692a
Tarun <taruncv516@gmail.com>
2023-04-17 23:49:07 +0530

    My-Commit

--- src/main/java/com/groupnine/travelbuddy/HelloServlet.java
+++ src/main/java/com/groupnine/travelbuddy/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Welcome to Travel Buddy Project!";
+        message = "Welcome to Travel Buddy";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -21,7 +21,6 @@ public class HelloServlet extends HttpServlet {
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
     }
-
     public void destroy() {
     }
 }
