fe91f865360ff9d197911c8581931ea9d8938b25
pranavnamburi18 <sp4siddhipranav@gmail.com>
2023-04-18 00:00:54 +0530

    000

--- src/main/java/com/groupnine/travelbuddy/HelloServlet.java
+++ src/main/java/com/groupnine/travelbuddy/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Welcome to Travel";
+        message = "Welcome to Pranav Travels";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
